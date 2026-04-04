216b8a314d882a4b5fdf1723aa2ca06aac0ab808
mhhong <mh.hong@proton.me>
2026-04-04 12:05:20 +0900

    Refactor: Create OpenAI object and pass it to QueryAgent - cleaner architecture | Add print for process tracking

--- job_pipeline.py
+++ job_pipeline.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 from typing import Optional
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from agents.query_agent import QueryAgent
 
 @dataclass
 class JobPipeline:
@@ -7,5 +11,16 @@ class JobPipeline:
     Orchestrate the main workflow of LLM agents and functions for the job discovery automation
     """
 
+    load_dotenv()
+
     def run(self):
-        pass
+        print("The main program started running")
+
+        client = OpenAI()
+        print(f"OpenAI model was loaded successfully: {client}")
+
+        query_agent = QueryAgent(client)
+        serp_queries, exa_queries = query_agent.get_queries()
+
+        print(f"SERP Len: {len(serp_queries)} | Queries: {serp_queries}")
+        print(f"EXA len: {len(exa_queries)} | Queries: {exa_queries}")
