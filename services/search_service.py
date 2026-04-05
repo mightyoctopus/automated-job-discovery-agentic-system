5fb0b03cb19047994239ade79e2642c26598c0f2
mhhong <mh.hong@proton.me>
2026-04-05 23:17:30 +0900

    Add serp and exa implementations

--- services/search_service.py
+++ services/search_service.py
@@ -1,19 +1,39 @@
+import time
 
 class SearchService:
     """
     Provide web search tools via SerpAPI and Exa API for fetching job results on the internet
     """
 
-    def __init__(self, serp, exa):
+    def __init__(self, serp, exa, serp_queries, exa_queries):
         self.serp_client = serp
         self.exa_client = exa
+        self.serp_queries = serp_queries
+        self.exa_queries = exa_queries
 
 
     def serpapi_web_search(self):
         """
         Search on the major search engines using SerpAPI for the SERPs data from Google, Bing, Yahoo, etc.
         """
-        pass
+
+        all_results = []
+        for i, q in enumerate(self.serp_queries):
+            response = self.serp_client({
+                "engine": "google",
+                "google_domain": "google.com",
+                "hl": "en",
+                "q": q
+            })
+
+            all_results.extend([
+                {**item, "searched_via": "serp"}
+                for item in response.get("organic_results", [])
+            ])
+            print(f"SerpAPI Search Interation {i + 1} Finished")
+            time.sleep(1)
+
+        return all_results
 
 
     def exa_web_search(self):
@@ -24,4 +44,26 @@ class SearchService:
         - long-tail or niche pages
         - results not easily surfaced via strict keyword search
         """
-        pass
+        all_exa_results = []
+
+        for i, q in enumerate(self.exa_queries):
+            response = self.exa_client.search(
+                query=q,
+                type="auto",
+                contents={"highlights": {"max_characters": 4000}}
+            )
+            print(f"Exa Search Iteration {i + 1} Finished")
+
+            all_exa_results.extend([
+                {
+                    "title": result.title,
+                    "link": result.url,
+                    "text": result.highlights[0] if result.highlights else None,
+                    "searched_via": "exa"
+                }
+                for result in response.results
+            ])
+            time.sleep(1)
+
+        return all_exa_results
+
