78878412cbf8998ff8d929b808956837183b9c51
mhhong <mh.hong@proton.me>
2026-04-07 16:40:09 +0900

    Build DataNormalizer class (complete)

---
+++ processors/data_normalizer.py
@@ -0,0 +1,31 @@
+from job import Job
+
+class DataNormalizer:
+    """
+    Normalize data from web search APIs and flatten their data structure
+    """
+
+    def __init__(self, serp_results, exa_results):
+        self.serp_data = serp_results
+        self.exa_data = exa_results
+
+    def normalize_job_data(self):
+        """
+        Normalize SerpAPI search result data into Job data schema
+        """
+
+        jobs = []
+
+        for item in self.serp_data + self.exa_data:
+            job = Job(
+                title=item.get("title", None),
+                url=item.get("link", None),
+                text=item.get("text", None), #SerpAPI doesn't return text(Job Description) so it requires to web-scrape later on
+                searched_via=item.get("searched_via", None)
+            )
+            print("Data conversion completed")
+            jobs.append(job)
+
+        print(f"Total jobs collected: {len(jobs)} jobs")
+        return jobs
+
