67b645d1b288f1d83c22a212f11f2225d67c2d7f
mhhong <mh.hong@proton.me>
2026-04-09 20:16:55 +0900

    Build WebScraper class

---
+++ services/web_scraper.py
@@ -0,0 +1,47 @@
+from typing import List
+import requests
+import time
+import random
+from job import Job
+from bs4 import BeautifulSoup
+
+class WebScraper:
+    """
+    Web-scrape job description for job objects without description data (job.text = None)
+    """
+
+    def __init__(self, jobs):
+        self.filtered_jobs = jobs
+
+
+    def web_scrape(self) -> List[Job]:
+
+        scrapped_jobs = []
+        for i, job in enumerate(self.filtered_jobs):
+            if not job.text:
+                try:
+                    response = requests.get(job.url, timeout=5)
+
+                    if response.status_code != 200:
+                        print(f"JOB ID {i + 1}: Failed with status {response.status_code}")
+                    else:
+                        soup = BeautifulSoup(response.text, "lxml")
+                        text = soup.get_text(separator="\n\n", strip=True)
+
+                        if text:
+                            # Assign the scraped text into the text attribute of Job object
+                            job.text = text
+
+                except requests.exceptions.RequestException as e:
+                    print(f"JOB ID {i + 1}: Failed to scrape - {e}")
+
+                # Small random delay (always)
+                time.sleep(random.uniform(1, 1.5))
+
+                # Batch pause (longer delay) every 15 jobs
+                if (i + 1) % 15 == 0:
+                    time.sleep(random.uniform(2, 3))
+
+            scrapped_jobs.append(job)
+
+        return scrapped_jobs
