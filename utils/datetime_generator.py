769d168ab521af5fd2bbc4eba9da507c1e254caf
mhhong <mh.hong@proton.me>
2026-04-18 02:25:14 +0900

    Fix the datetime generator fucntion for the date type issue -- make it generate date data in string type

--- utils/datetime_generator.py
+++ utils/datetime_generator.py
@@ -2,5 +2,5 @@ from datetime import datetime, timezone
 
 
 def generate_current_datetime():
-    time_now = datetime.now(timezone.utc)
-    return time_now.date()
+    time_now = datetime.now(timezone.utc).date().isoformat()
+    return time_now
