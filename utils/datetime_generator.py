2136604755041dca9b7b96475d66494ced4abecd
mhhong <mh.hong@proton.me>
2026-04-25 01:49:31 +0900

    Add search_offset_time util function used for search range settings on Exa

--- utils/datetime_generator.py
+++ utils/datetime_generator.py
@@ -1,6 +1,16 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 def generate_current_datetime():
     time_now = datetime.now(timezone.utc).date().isoformat()
     return time_now
+
+
+def generate_search_offset_time():
+    """
+    Generate datetime used for the offset time of search range
+    """
+    current_time = datetime.now(timezone.utc)
+    five_weeks_ago = current_time - timedelta(weeks=5)
+
+    return five_weeks_ago.isoformat()
